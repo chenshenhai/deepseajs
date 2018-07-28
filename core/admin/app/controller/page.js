b1aed7beef3a237c6feb7558e7c13306906a9322
chenshenhai <cshenhai@gmail.com>
2018-07-28 15:19:11 +0800

    feat: add admin

---
+++ core/admin/app/controller/page.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  async index(ctx) {
+    ctx.body = 'This is index page!';
+  },
+
+  async hello(ctx) {
+    ctx.body = 'This is hello page!';
+  }
+};
---
+++ core/admin/index.js
@@ -0,0 +1,24 @@
+'use strict';
+const Application = require('./lib/application');
+
+// 初始化Egg.js应用
+const app = new Application({
+  baseDir: __dirname,
+  type: 'application'
+});
+
+// 应用启动
+app.ready(err => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  const server = require('http').createServer(app.callback());
+  server.once('error', err => {
+    console.log('[app_worker] server got error: %s, code: %s', err.message, err.code);
+    process.exit(1);
+  });
+  server.listen(7001, () => {
+    console.log('server started at 7001');
+  });
+});
