6a1f5bb275ac8cb9be757400fd7d1048c7ca3e6c
chenshenhai <cshenhai@gmail.com>
2018-11-22 23:54:07 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 const http = require('http');
-const ThemeServer = require('./sys/theme-server/index');
+const serverPortal = require('./sys/server-portal/index');
+const serverDashboard = require('./sys/server-portal/index');
 
-const app = new ThemeServer({
-  baseDir: __dirname,
-  themeName: 'blog',
-  dataHub: {
-    getName: () => 'theme server',
-    getInfo: () => {
-      return {
-        title: 'theme',
-        datalist: ['001', '002']
-      };
-    }
-  }
-});
-
-const server = http.createServer(app.callback());
-server.once('error', err => {
+const portal = http.createServer(serverPortal.callback());
+portal.once('error', err => {
   console.log(' server got error: %s, code: %s', err.message, err.code);
   process.exit(1);
 });
-server.listen(3001, () => {
+portal.listen(3000, () => {
+  console.log('server started at 3000');
+});
+
+const dashboard = http.createServer(serverDashboard.callback());
+dashboard.once('error', err => {
+  console.log(' server got error: %s, code: %s', err.message, err.code);
+  process.exit(1);
+});
+dashboard.listen(3001, () => {
   console.log('server started at 3001');
 });
---
+++ sys/server-dashboard/theme/dashboard/page/index/index.js
@@ -0,0 +1,14 @@
+ThemePage({
+  data: {
+    title: 'hello page',
+    text: 'this is hello page!',
+    isShowDataList: true,
+    datalist: [
+      '001', '002', '003', '004'
+    ],
+    datajson: {
+      'key1': 'val1',
+      'key2': 'val2'
+    }
+  }
+});
