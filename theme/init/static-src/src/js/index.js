a9cf88a999043559d36ba3a8d5b4ae5903c8b483
chenshenhai <cshenhai@gmail.com>
2018-12-04 21:08:02 +0800

    feat: update theme init static

--- theme/init/static-src/src/js/index.js
+++ theme/init/static-src/src/js/index.js
@@ -1,95 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { Steps, Button, message, Layout, Menu } from 'antd';
+
 import './../css/index.less';
 
 const { Header, Content, Footer } = Layout;
-const container = document.getElementById('PageApp');
-const Step = Steps.Step;
 
-const steps = [{
-  title: 'Storage type',
-  content: 'Second-content'
-}, {
-  title: 'Super admin',
-  content: 'First-content'
-}, {
-  title: 'Server config',
-  content: 'Fourth-content'
-}, {
-  title: 'Start all server',
-  content: 'Last-content'
-}];
+const Language = () => <h2>Language</h2>;
+const StepProcess = () => <h2>StepProcess</h2>;
+const Info = () => <h2>Info</h2>;
 
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      current: 0
-    };
-  }
-
-  next () {
-    const current = this.state.current + 1;
-    this.setState({ current });
-  }
-
-  prev () {
-    const current = this.state.current - 1;
-    this.setState({ current });
-  }
-
-  render () {
-    const { current } = this.state;
-    return (
-      <div className="page-app-container">
-        <Steps current={current} >
-          {steps.map(item => <Step key={item.title} title={item.title} />)}
-        </Steps>
-        <div className="app-steps-content">{steps[current].content}</div>
-        <div className="app-steps-action">
-          {
-            current < steps.length - 1 &&
-            <Button type="primary" onClick={() => this.next()}>Next</Button>
-          }
-          {
-            current === steps.length - 1 &&
-            <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
-          }
-          {
-            current > 0 &&
-            (
-              <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-              Previous
-              </Button>
-            )
-          }
+function AppLink ({ label, to, activeOnlyWhenExact }) {
+  return (
+    <Route
+      path={to}
+      exact={activeOnlyWhenExact}
+      children={({ match }) => (
+        <div className={match ? 'active' : ''}>
+          {match ? '> ' : ''}
+          <Link to={to}>{label}</Link>
         </div>
-      </div>
-    );
-  }
+      )}
+    />
+  );
 }
 
+const AppRouter = () => (
+  <Router>
+    <div>
+      <Layout className="layout">
+        <Header style={{ height: '40px' }}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            style={{ height: '40px', lineHeight: '40px' }}
+          >
+            <Menu.Item key="1">
+              <AppLink activeOnlyWhenExact={true} to='/' label="Language" />
+            </Menu.Item>
+            <Menu.Item key="2">
+              <AppLink to='/step' label="StepProcess" />
+            </Menu.Item>
+            <Menu.Item key="3">
+              <AppLink to='/info' label="Info" />
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Route path='/' exact component={Language} />
+          <Route path='/step' component={StepProcess} />
+          <Route path='/info' component={Info} />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          toojs ©2018 Created by chenshenhai
+        </Footer>
+      </Layout>
+    </div>
+  </Router>
+);
+
+const container = document.getElementById('PageApp');
+
 ReactDOM.render(
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <App />
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      toojs ©2018 Created by chenshenhai
-    </Footer>
-  </Layout>,
+  <AppRouter />,
   container);
--- theme/init/static/js/index.js
+++ theme/init/static/js/index.js
@@ -1,1 +1,287 @@
-!function(u){function e(e){for(var t,n,r=e[0],o=e[1],a=e[2],c=0,i=[];c<r.length;c++)n=r[c],s[n]&&i.push(s[n][0]),s[n]=0;for(t in o)Object.prototype.hasOwnProperty.call(o,t)&&(u[t]=o[t]);for(p&&p(e);i.length;)i.shift()();return f.push.apply(f,a||[]),l()}function l(){for(var e,t=0;t<f.length;t++){for(var n=f[t],r=!0,o=1;o<n.length;o++){var a=n[o];0!==s[a]&&(r=!1)}r&&(f.splice(t--,1),e=c(c.s=n[0]))}return e}var n={},s={0:0},f=[];function c(e){if(n[e])return n[e].exports;var t=n[e]={i:e,l:!1,exports:{}};return u[e].call(t.exports,t,t.exports,c),t.l=!0,t.exports}c.m=u,c.c=n,c.d=function(e,t,n){c.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},c.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},c.t=function(t,e){if(1&e&&(t=c(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(c.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)c.d(n,r,function(e){return t[e]}.bind(null,r));return n},c.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return c.d(t,"a",t),t},c.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},c.p="";var t=window.webpackJsonp=window.webpackJsonp||[],r=t.push.bind(t);t.push=e,t=t.slice();for(var o=0;o<t.length;o++)e(t[o]);var p=r;f.push([88,1]),l()}({88:function(e,t,n){"use strict";n.r(t);var r=n(30),o=n.n(r),a=n(83),c=n.n(a),i=n(39),u=n.n(i),l=n(59),s=n.n(l),f=n(29),p=n.n(f),y=n(1),m=n.n(y),v=n(6),b=n.n(v);n(93);function d(e){return(d="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e})(e)}function h(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function g(e,t){return!t||"object"!==d(t)&&"function"!=typeof t?function(e){if(void 0!==e)return e;throw new ReferenceError("this hasn't been initialised - super() hasn't been called")}(e):t}function E(e){return(E=Object.setPrototypeOf?Object.getPrototypeOf:function(e){return e.__proto__||Object.getPrototypeOf(e)})(e)}function S(e,t){return(S=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}var O=p.a.Header,j=p.a.Content,w=p.a.Footer,k=document.getElementById("PageApp"),P=s.a.Step,_=[{title:"Storage type",content:"Second-content"},{title:"Super admin",content:"First-content"},{title:"Server config",content:"Fourth-content"},{title:"Start all server",content:"Last-content"}],x=function(e){function n(e){var t;return function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),(t=g(this,E(n).call(this,e))).state={current:0},t}var t,r,o;return function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function");e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,writable:!0,configurable:!0}}),t&&S(e,t)}(n,m.a.Component),t=n,(r=[{key:"next",value:function(){var e=this.state.current+1;this.setState({current:e})}},{key:"prev",value:function(){var e=this.state.current-1;this.setState({current:e})}},{key:"render",value:function(){var e=this,t=this.state.current;return m.a.createElement("div",{className:"page-app-container"},m.a.createElement(s.a,{current:t},_.map(function(e){return m.a.createElement(P,{key:e.title,title:e.title})})),m.a.createElement("div",{className:"app-steps-content"},_[t].content),m.a.createElement("div",{className:"app-steps-action"},t<_.length-1&&m.a.createElement(u.a,{type:"primary",onClick:function(){return e.next()}},"Next"),t===_.length-1&&m.a.createElement(u.a,{type:"primary",onClick:function(){return c.a.success("Processing complete!")}},"Done"),0<t&&m.a.createElement(u.a,{style:{marginLeft:8},onClick:function(){return e.prev()}},"Previous")))}}])&&h(t.prototype,r),o&&h(t,o),n}();b.a.render(m.a.createElement(p.a,{className:"layout"},m.a.createElement(O,null,m.a.createElement("div",{className:"logo"}),m.a.createElement(o.a,{theme:"dark",mode:"horizontal",defaultSelectedKeys:["2"],style:{lineHeight:"64px"}},m.a.createElement(o.a.Item,{key:"1"},"nav 1"),m.a.createElement(o.a.Item,{key:"2"},"nav 2"),m.a.createElement(o.a.Item,{key:"3"},"nav 3"))),m.a.createElement(j,{style:{padding:"0 50px"}},m.a.createElement(x,null)),m.a.createElement(w,{style:{textAlign:"center"}},"toojs ©2018 Created by chenshenhai")),k)},93:function(e,t,n){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"index": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./static-src/src/js/index.js","vendors"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./static-src/src/css/index.less":
+/*!***************************************!*\
+  !*** ./static-src/src/css/index.less ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./static-src/src/js/index.js":
+/*!************************************!*\
+  !*** ./static-src/src/js/index.js ***!
+  \************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var antd_lib_menu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! antd/lib/menu */ "./node_modules/_antd@3.10.9@antd/lib/menu/index.js");
+/* harmony import */ var antd_lib_menu__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(antd_lib_menu__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var antd_lib_layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! antd/lib/layout */ "./node_modules/_antd@3.10.9@antd/lib/layout/index.js");
+/* harmony import */ var antd_lib_layout__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(antd_lib_layout__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "./node_modules/_react@16.6.3@react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-dom */ "./node_modules/_react-dom@16.6.3@react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-router-dom */ "./node_modules/_react-router-dom@4.3.1@react-router-dom/es/index.js");
+/* harmony import */ var _css_index_less__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./../css/index.less */ "./static-src/src/css/index.less");
+/* harmony import */ var _css_index_less__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_css_index_less__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
+
+
+
+var Header = antd_lib_layout__WEBPACK_IMPORTED_MODULE_1___default.a.Header,
+    Content = antd_lib_layout__WEBPACK_IMPORTED_MODULE_1___default.a.Content,
+    Footer = antd_lib_layout__WEBPACK_IMPORTED_MODULE_1___default.a.Footer;
+
+var Language = function Language() {
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("h2", null, "Language");
+};
+
+var StepProcess = function StepProcess() {
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("h2", null, "StepProcess");
+};
+
+var Info = function Info() {
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("h2", null, "Info");
+};
+
+function AppLink(_ref) {
+  var label = _ref.label,
+      to = _ref.to,
+      activeOnlyWhenExact = _ref.activeOnlyWhenExact;
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["Route"], {
+    path: to,
+    exact: activeOnlyWhenExact,
+    children: function children(_ref2) {
+      var match = _ref2.match;
+      return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
+        className: match ? 'active' : ''
+      }, match ? '> ' : '', react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["Link"], {
+        to: to
+      }, label));
+    }
+  });
+}
+
+var AppRouter = function AppRouter() {
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["HashRouter"], null, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(antd_lib_layout__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    className: "layout"
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(Header, {
+    style: {
+      height: '40px'
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
+    className: "logo"
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(antd_lib_menu__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    theme: "dark",
+    mode: "horizontal",
+    style: {
+      height: '40px',
+      lineHeight: '40px'
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(antd_lib_menu__WEBPACK_IMPORTED_MODULE_0___default.a.Item, {
+    key: "1"
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(AppLink, {
+    activeOnlyWhenExact: true,
+    to: "/",
+    label: "Language"
+  })), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(antd_lib_menu__WEBPACK_IMPORTED_MODULE_0___default.a.Item, {
+    key: "2"
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(AppLink, {
+    to: "/step",
+    label: "StepProcess"
+  })), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(antd_lib_menu__WEBPACK_IMPORTED_MODULE_0___default.a.Item, {
+    key: "3"
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(AppLink, {
+    to: "/info",
+    label: "Info"
+  })))), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(Content, {
+    style: {
+      padding: '0 50px'
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["Route"], {
+    path: "/",
+    exact: true,
+    component: Language
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["Route"], {
+    path: "/step",
+    component: StepProcess
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__["Route"], {
+    path: "/info",
+    component: Info
+  })), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(Footer, {
+    style: {
+      textAlign: 'center'
+    }
+  }, "toojs \xA92018 Created by chenshenhai"))));
+};
+
+var container = document.getElementById('PageApp');
+react_dom__WEBPACK_IMPORTED_MODULE_3___default.a.render(react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(AppRouter, null), container);
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=index.js.map
